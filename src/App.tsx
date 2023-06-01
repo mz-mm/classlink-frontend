93624b54daa04997e1cc9bdb91ff0028fe42636e
mzdevI <muhammed.mammadhuseynov@gmail.com>
2023-06-01 11:12:55 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import {Routes, Route} from "react-router-dom";
-import NotFound from "./components/notFound.tsx";
-import Login from "./components/login.tsx";
-import Dashboard from "./components/dashboard/dashboard.tsx";
+import {Route, Routes} from 'react-router-dom';
+import NotFound from './components/notFound.tsx';
+import Login from './components/login.tsx';
+import Dashboard from './components/dashboard/dashboard.tsx';
 
 function App() {
-
     return (
         <Routes>
             <Route path="/" element={<Login/>}/>
             <Route path="/dashboard" element={<Dashboard/>}/>
             <Route path="*" element={<NotFound/>}/>
         </Routes>
-    )
+    );
 }
 
-export default App
+export default App;
--- src/services/api-client.tsx
+++ src/services/api-client.tsx
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "http://127.0.0.1:8000/api",
 });
 
-// Add an axios interceptor to update the Authorization header before each request is sent
 apiClient.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
